3182013eab714902824c4df32f7a8e0c23fe3b47
razznblue <kaipojames12@gmail.com>
2023-10-06 09:31:37 -0500

    Extend Login Page UI

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,16 +1,51 @@
 import MainContainer from "../MainContainer/MainContiner";
 import LittleButton from "../LittleButton/LittleButton";
+import Image from "next/image";
+import { CSSProperties } from "react";
 
+const contentStyles: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '80vh',
+}
 
-/**
- * Login PAGE
- */
+const imgContainerStyles: CSSProperties = {
+  display: 'flex',
+  justifyContent: 'center',
+  width: '30%'
+}
+
+const hStyles: CSSProperties = {
+  color: '#FFF',
+  textAlign: 'center',
+  fontSize: 'max(6.5vw, 30px)',
+  margin: '0',
+  padding: '0'
+}
+
+const h2Styles: CSSProperties = {
+  ...hStyles,
+  marginTop: '-5%'
+}
+
 const Login = ({onClick}) => {
   return (
     <MainContainer title="SWGU | Sign In" description="Sign In Page of SWGU" bgImage="plain-background" lineArt="double">
+      <div style={contentStyles} className="content">
+        <div style={imgContainerStyles} className="img-container">
+          <Image style={{
+            width: '100%',
+            height: 'auto'
+          }} src='/images/app-icon-1024-transparent.png' alt='' width='50' height='50' />
+        </div>
+        <h1 style={hStyles}>GALAXY</h1>
+        <h2 style={h2Styles}>ULTIMATE TCG</h2>
+      </div>
       <LittleButton text="Sign In" imageDirection="right" onClick={onClick} />
     </MainContainer>
   );
 };
 
-export default Login;
+export default Login
