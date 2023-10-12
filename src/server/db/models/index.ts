fc5a7570f88f4b55daaa903b5229bed9537c681c
razznblue <kaipojames12@gmail.com>
2023-10-12 09:06:02 -0500

    Export DeckModel

--- src/server/db/models/index.ts
+++ src/server/db/models/index.ts
@@ -1,12 +1,14 @@
 import { getModelForClass } from '@typegoose/typegoose';
 import { Card } from './Card';
 import { Location } from './Location';
+import { Deck } from './Deck';
 import { Player } from './Player';
 import { PlayerStats } from './PlayerStats';
 import { PlayerCollection } from './PlayerCollection';
 
 export const CardModel = getModelForClass(Card);
 export const LocationModel = getModelForClass(Location);
+export const DeckModel = getModelForClass(Deck);
 export const PlayerModel = getModelForClass(Player);
 export const PlayerStatsModel = getModelForClass(PlayerStats);
 export const PlayerCollectionModel = getModelForClass(PlayerCollection);
