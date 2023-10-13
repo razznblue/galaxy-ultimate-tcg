eb9f7778ad6ef0534e484eec7c4ac0e7fc07d0a3
razznblue <59562158+razznblue@users.noreply.github.com>
2023-10-13 10:32:06 -0500

    Update Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,6 +3,8 @@ import LittleButton from "../LittleButton/LittleButton";
 import Image from "next/image";
 import { CSSProperties } from "react";
 import InstallPWA from "../PWA/installBtn";
+import { motion } from "framer-motion";
+import Text from "../Text/Text";
 
 const contentStyles: CSSProperties = {
   display: 'flex',
@@ -26,7 +28,6 @@ const hStyles: CSSProperties = {
   margin: '0',
   padding: '0'
 }
-
 const h2Styles: CSSProperties = {
   ...hStyles,
   marginTop: '-5%'
@@ -36,14 +37,20 @@ const Login = ({onClick}) => {
   return (
     <MainContainer title="SWGU | Sign In" description="Sign In Page of SWGU" bgImage="plain-background" lineArt="double">
       <div style={contentStyles} className="content">
-        <div style={imgContainerStyles} className="img-container">
+        <motion.div 
+          initial={{ opacity: 0, y: -60 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }} 
+          transition={{ type: "spring", stiffness: 100 }}
+          style={imgContainerStyles} 
+          className="img-container">
           <Image style={{
             width: '100%',
             height: 'auto'
           }} src='/images/app-icon-1024-transparent.png' alt='' width='50' height='50' priority={true} />
-        </div>
-        <h1 style={hStyles}>GALAXY</h1>
-        <h2 style={h2Styles}>ULTIMATE TCG</h2>
+        </motion.div>
+        <Text text="Galaxy" customStyle={hStyles} animationType='spring' />
+        <Text text="Ultimate TCG" customStyle={h2Styles} animationType='spring' />
         <InstallPWA text={'Install'} placement={'default'}/>
       </div>
       <LittleButton text="Login" imageDirection="right" onClick={onClick} />
