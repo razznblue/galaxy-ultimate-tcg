9a0790a2cfef5716fd58235ec7bc993d5ed539f5
razznblue <kaipojames12@gmail.com>
2023-10-19 10:42:01 -0500

    Code cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,19 @@
 import type { NextPage } from 'next'
 import { signIn, useSession } from 'next-auth/react';
-import { DefaultSession } from 'next-auth';
 import Home from '../components/Home/Home';
 import Login from '../components/Login/Login';
 
 
-const HomePage: NextPage = ({user}: {user: DefaultSession["user"]}) => {
+const HomePage: NextPage = () => {
   const { data: session } = useSession()
 
   const signInUser = () => {
     if (!session) signIn('google')
   }
 
-  if (session) {
-    return <Home user={session.user} />
-  }
-  if (user) {
-    console.log('DefaultSession:');
-  } 
-  return <Login onClick={signInUser} />
+  return session 
+    ? <Home user={session.user} />
+    : <Login onClick={signInUser} />
 }
 
 export default HomePage
