2e48d79c4d4f08fe7ea65f045350856a39185596
razznblue <kaipojames12@gmail.com>
2023-10-19 11:02:03 -0500

    Add Loading Component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import MainContainer from '../MainContainer/MainContiner';
+
+const Loading = () => {
+  return (
+    
+    <MainContainer title='SWGU | LOADING' lineArt='double'>
+      <style jsx>{`
+          .loader {
+            border: 16px solid #f3f3f3; 
+            border-top: 16px solid #3498db;
+            border-radius: 50%;
+            width: 120px;
+            height: 120px;
+            animation: spin 2s linear infinite;
+            margin: auto;
+          }
+
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+      `}</style>
+      <div className="loader"></div> 
+    </MainContainer>
+  );
+}
+
+export default Loading;
