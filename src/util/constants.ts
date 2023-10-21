d63f20dc881efe62098c8bd1a3ff0f5c3193bad1
razznblue <kaipojames12@gmail.com>
2023-10-21 10:52:43 -0500

    semicolon

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,2 +1,2 @@
-export const assetServiceUrl = "https://asset-service-drive.onrender.com";
-export const defaultAvatarImg = `${assetServiceUrl}/IMAGES/backgrounds/default-profile.png`;
+export const assetServiceUrl = "https://asset-service-drive.onrender.com"
+export const defaultAvatarImg = `${assetServiceUrl}/IMAGES/backgrounds/default-profile.png`
